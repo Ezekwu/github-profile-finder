865e103219cf30ece23a5622843471e44cdff8b7
jeremiah <100346840+Ezekwu@users.noreply.github.com>
2022-07-11 14:11:50 -0700

    Update UserSearch.jsx

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import { FaSearch } from 'react-icons/fa'
+import { useState, useContext } from 'react'
+import GithubContext from '../../context/github/GithubContext'
+
 const UserSearch = () => {
+    const [text, setText] = useState('');
+    const {users} = useContext(GithubContext)
+    
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if(text === ''){
+            alert('please type something')
+        } else{
+            setText('')
+        }
+    }
     return (
-        <div className='flex flex-col  space-x-6 w-4/5  lg:flex-row '>
-            <form className='flex items-center'>
-                <input type="text" className='input bg-gray-200 text-black rounded-r-none lg:w-80'/>
-                <button className='btn rounded-l-none w-20' ><FaSearch /> </button>
+        <div className='flex flex-col  space-x-6 w-4/5 items-start lg:flex-row '>
+            <form className='flex items-center '
+            onSubmit={handleSubmit}>
+                <input type="text" 
+                placeholder='search'
+                className='input bg-gray-200 text-black w-60 rounded-r-none  lg:w-80 '
+                value={text}
+                onChange={handleChange}/>
+                <button className='btn rounded-l-none w-20' type='submit' ><FaSearch /> </button>
             </form>
-            <button className='btn btn-ghost'>clear</button>
+
+            {users.length > 0 && <button className='btn btn-ghost'>clear</button>}
+            
         </div>
     )
 }
